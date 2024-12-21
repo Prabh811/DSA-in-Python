dd967e39922492edbef767f2c69eda622aed8221
Prabhakar <prabhakar810nit@gmail.com>
2024-12-21 16:22:49 +0530

    Used while loop

--- Print all numbers 1 to n.py
+++ Print all numbers 1 to n.py
@@ -1,7 +1,21 @@
 user_input = int(input("Enter a number and this program will print from 1 to n: "))
 
-try:
-    for i in range(1, user_input + 1):
-        print(i)
-except Exception as e:
-    print(e)
+# Using for loop
+
+# try:
+#     for i in range(1, user_input + 1):
+#         print(i)
+# except Exception as e:
+#     print(e)
+
+
+# Using while loop
+
+# i = 1
+# try:
+#     while(i <= user_input):
+#         print(i)
+#         i += 1
+# except Exception as e:
+#     print(e)
+        
